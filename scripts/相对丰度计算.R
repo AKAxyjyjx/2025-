66ae208300d1c7ac24a995b05231ddd65e5ed533
AKAxyjyjx <Chenyy.angel@qq.com>
2026-07-15 23:29:50 +0800

    Update 相对丰度计算.R

--- scripts/相对丰度计算.R
+++ scripts/相对丰度计算.R
@@ -1,5 +1,5 @@
 
-asv4 <- read.table("asv_table_only.csv", header=T, sep=",", stringsAsFactors = FALSE) 
+asv4 <- read.table("temperary_data/asv_table_only.csv", header=T, sep=",", stringsAsFactors = FALSE) 
 asv4 <- rbind(asv4, colSums(asv4))
 for (i in 1:ncol(asv4)) {
   asv4[, i] <- asv4[, i] / asv4[nrow(asv4), i]
@@ -12,6 +12,6 @@ sum <- colSums(asv4)
 sum
 
 # 将相对丰度表导出，导出后记得打开表格手动调整
-write.table(asv4, "relative_table_only.csv", sep = "\t", quote = F)
+write.table(asv4, "temperary_data/relative_table_only.csv", sep = "\t", quote = F)
 
 #生成的relative表格第一行也有错位，需要调整
