05207ef228860cd4dfe98d48d903545110356c1e
AKAxyjyjx <Chenyy.angel@qq.com>
2026-07-15 23:30:14 +0800

    Update file paths for reading and writing data

--- scripts/统计合并作业.R
+++ scripts/统计合并作业.R
@@ -1,7 +1,7 @@
 
 # 读取
-asv_table <- read.csv("relative_table_only.csv")
-taxonomy_table <- read.csv("taxonomy_table.csv")
+asv_table <- read.csv("temperary_data/relative_table_only.csv")
+taxonomy_table <- read.csv("temperary_data/taxonomy_table.csv")
 
 #asv_table <-asv4
 #taxonomy_table 
@@ -14,4 +14,4 @@ merged <- left_join(taxonomy_table, asv_table, by = "ASV_ID")
 # 查看
 head(merged[, 1:10])
 
-write.table(merged, "relative_table_tax.csv", sep = "\t", quote = F)
+write.table(merged, "temperary_data/relative_table_tax.csv", sep = "\t", quote = F)
